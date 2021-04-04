8dcc630aacae33ce81289cf288df28f03038a7b2
Kefirkayaky <alexkut2611@gmail.com>
2021-04-04 14:37:55 +0300

    ,;;

--- brick.py
+++ brick.py
@@ -34,14 +34,54 @@ def Taverna(hero):
     pass
 
 def Peshera(hero):
-    print("Куда пойдёшь \n 1 - На право 2 - на лево ")
-    if input() == "1":
-        print("Вы выбрали на право")
+    for i in range(10):
+        print("Куда пойдёшь \n 1 - лево  2 - на право 3 - вперед ")
+        storona =  input()
 
-        print("Эпичная Музычка")
-        isPererojdenie = draka(hero,gul)
-        if isPererojdenie == True:
-            Brick(hero)
+        gul = Monstr()
+
+        gul.randompower = 30
+        gul.critpower = 80
+        gul.critchance = 19
+        gul.power = 60
+        gul.HP = 200
+        gul.superpower = 0
+        if storona==1:
+            levo(hero,gul,"zoloto")
+        elif  storona==2:
+            pravo(hero,gul,"zoloto")
+        else:
+            pryamo(hero,gul,"zoloto")
+
+
+
+    # print("Куда пойдёшь \n 1 - На право 2 - на лево ")
+    # if input() == "1":
+    #     print("Вы выбрали на право")
+    #
+    #     print("Эпичная Музычка")
+    #     isPererojdenie = draka(hero,gul)
+    #     if isPererojdenie == True:
+    #         Brick(hero)
+    #
+    # else:
+    #     print("Вы выбрали на лево")
+    #     print("Перед вами золото: \n 1 - Добыть 2 - Оставить")
+    #     if input()=="1":
+    #         print("Вы Добыли золото")
+    #         #todo Сумка+1
+    #     else:
+    #         print("Вы отказались")
+
+
+        # todo драка
+def levo(hero,monstr,item):
+
+    print("Вы выбрали на лево")
+    print("Эпичная Музычка")
+    isPererojdenie = draka(hero,gul)
+    if isPererojdenie == True:
+        Brick(hero)
 
     else:
         print("Вы выбрали на лево")
@@ -52,32 +92,44 @@ def Peshera(hero):
         else:
             print("Вы отказались")
 
-        gul = Monstr()
-
-        gul.randompower = 30
-        gul.critpower = 80
-        gul.critchance = 19
-        gul.power = 60
-        gul.HP = 200
-        gul.superpower = 0
-        levo(hero,gul,"zoloto")
-        # todo драка
-def levo(hero,monstr,item):
-    print("Вы выбрали на лево")
 
 
 
 
-
-    print("Куда пойдёшь \n 1 - Вперёд  2 - на лево ")
 
 def pravo(hero,monstr,item):
-    print("Вы выбрали на право")
 
+    print("Вы выбрали на право")
+    print("Эпичная Музычка")
+    isPererojdenie = draka(hero,gul)
+    if isPererojdenie == True:
+        Brick(hero)
+
+    else:
+        print("Вы выбрали на лево")
+        print("Перед вами золото: \n 1 - Добыть 2 - Оставить")
+        if input()=="1":
+            print("Вы Добыли золото")
+            #todo Сумка+1
+        else:
+            print("Вы отказались")
 
 
 def pryamo(hero,monstr,item):
+    print("Вы выбрали на право")
+    print("Эпичная Музычка")
+    isPererojdenie = draka(hero, gul)
+    if isPererojdenie == True:
+        Brick(hero)
+
+    else:
+        print("Вы выбрали на лево")
+        print("Перед вами золото: \n 1 - Добыть 2 - Оставить")
+        if input() == "1":
+            print("Вы Добыли золото")
+            # todo Сумка+1
+        else:
+            print("Вы отказались")
 
 
-    print("Куда пойдёшь \n 1 - Вперёд  2 - на лево ")
     print("Вы выбрали вперед")
